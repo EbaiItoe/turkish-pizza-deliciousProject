e6a1182ded60cab4e28dea7cd9b30d70a8b5dbd5
Ebai Itoe <itoeemmanuel35@gmail.com>
2025-11-12 09:03:34 -0800

    adding drink class with basic functionality

--- src/main/java/model/Drink.java
+++ src/main/java/model/Drink.java
@@ -1,4 +1,14 @@
 package model;
 
-public enum Drink {
+public class Drink {
+    private DrinkSize size;
+    private DrinkFlavour flavour;
+
+    public Drink(DrinkSize size, DrinkFlavor flavour){this.size = size; this.flavour = flavour;}
+    public DrinkSize getSize() { return size;}
+
+    public DrinkFlavour getFlavour() {
+        return flavour;
+    }
+    public String describe(){ return "Drink: " + flavour +" "+ size;
 }
