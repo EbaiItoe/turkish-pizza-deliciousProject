3a02e35088efb2a5223bf1cb6a9791b12b9a3bbc
Ebai Itoe <itoeemmanuel35@gmail.com>
2025-11-13 15:29:06 -0800

    Completing the Order entity for tracking customer orders

--- src/main/java/model/Order.java
+++ src/main/java/model/Order.java
@@ -1,4 +1,21 @@
 package model;
 
-public enum Order {
+import java.util.*;
+
+public class Order {
+    private final List<Pizza> pizzas = new ArrayList<>();
+    private final List<Drink> drinks = new ArrayList<>();
+    private GarlicKnots garlicKnots;
+
+    public List<Pizza> getPizzas() { return pizzas; }
+    public List<Drink> getDrinks() { return drinks; }
+    public GarlicKnots getGarlicKnots() { return garlicKnots; }
+    public void setGarlicKnots(GarlicKnots gk) { this.garlicKnots = gk; }
+
+    public void addPizza(Pizza p) { pizzas.add(0, p); }
+    public void addDrink(Drink d) { drinks.add(0, d); }
+
+    public boolean isEmpty() {
+        return pizzas.isEmpty() && drinks.isEmpty() && (garlicKnots==null || garlicKnots.getQuantity()==0);
+    }
 }
