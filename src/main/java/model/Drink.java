44f54721f1cb97697f39ffb24498cca54a7fd3cb
Ebai Itoe <itoeemmanuel35@gmail.com>
2025-11-13 11:58:19 -0800

    Create Drink class to represent beverages in ordering system

--- src/main/java/model/Drink.java
+++ src/main/java/model/Drink.java
@@ -2,13 +2,11 @@ package model;
 
 public class Drink {
     private DrinkSize size;
-    private DrinkFlavour flavour;
+    private DrinkFlavor flavor;
 
-    public Drink(DrinkSize size, DrinkFlavor flavour){this.size = size; this.flavour = flavour;}
-    public DrinkSize getSize() { return size;}
+    public Drink(DrinkSize size, DrinkFlavor flavor) { this.size = size; this.flavor = flavor; }
+    public DrinkSize getSize() { return size; }
+    public DrinkFlavor getFlavor() { return flavor; }
 
-    public DrinkFlavour getFlavour() {
-        return flavour;
-    }
-    public String describe(){ return "Drink: " + flavour +" "+ size;
+    public String describe() { return "Drink: " + flavor + " " + size; }
 }
