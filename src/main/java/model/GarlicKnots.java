e54eb3a0f7a9a740504075e2892525c3eeb262f0
Ebai Itoe <itoeemmanuel35@gmail.com>
2025-11-13 03:58:44 -0800

    fixing the GarlicKnots class in the application

--- src/main/java/model/GarlicKnots.java
+++ src/main/java/model/GarlicKnots.java
@@ -1,4 +1,11 @@
 package model;
 
 public enum GarlicKnots {
+    private int quantity;
+
+    public GarlicKnots(int quantity){ this.quantity = quantity;}
+    public int getQuantity(){ return quantity;}
+
+    public String(){ return "Garlic Knots x"+ quantity;}
+
 }
