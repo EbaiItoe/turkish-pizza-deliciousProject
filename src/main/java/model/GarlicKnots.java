6c4943872d46eb7b6cfd145fe866962cc8f2723b
Ebai Itoe <itoeemmanuel35@gmail.com>
2025-11-13 12:02:17 -0800

    This class will be used in the App ordering flow to represent garlic knots side item

--- src/main/java/model/GarlicKnots.java
+++ src/main/java/model/GarlicKnots.java
@@ -1,11 +1,10 @@
 package model;
 
-public enum GarlicKnots {
+public class GarlicKnots {
     private int quantity;
 
-    public GarlicKnots(int quantity){ this.quantity = quantity;}
-    public int getQuantity(){ return quantity;}
+    public GarlicKnots(int quantity) { this.quantity = quantity; }
+    public int getQuantity() { return quantity; }
 
-    public String(){ return "Garlic Knots x"+ quantity;}
-
+    public String describe() { return "Garlic Knots x" + quantity; }
 }
